697133d25800826aba901106194ba86479bbcdf8
kadrianne <kristine.a.du@gmail.com>
2020-03-18 16:26:39 -0600

    Complete backend.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,22 @@
+require 'faker'
 class PokemonsController < ApplicationController
+
+    def index
+        @pokemons = Pokemon.all
+        render json: @pokemons
+    end
+
+    def create
+        @pokemon = Pokemon.create(
+            nickname: Faker::Name.first_name,
+            species: Faker::Games::Pokemon.name,
+            trainer_id: params[:trainer_id]
+            )
+        render json: @pokemon
+    end
+
+    def destroy
+        Pokemon.find(params[:id]).destroy
+    end
+
 end
