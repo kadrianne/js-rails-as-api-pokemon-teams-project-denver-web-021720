38b408086345852d93c35ce584bbf15288f144df
kadrianne <kristine.a.du@gmail.com>
2020-03-19 11:32:54 -0600

    Make dynamic cards.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(trainers => makeCards(trainers))
+
+function makeCards(trainers){
+    trainers.forEach(trainer => {
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = `<p>${trainer.name}</p>
+    <button>Add Pokemon</button>
+    <ul></ul>`
+    document.body.append(card)
+    })
+}
