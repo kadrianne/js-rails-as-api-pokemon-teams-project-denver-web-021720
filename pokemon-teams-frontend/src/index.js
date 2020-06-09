1279b201bfb3e7ce3cdf1b83d5d8a38e7009b813
kadrianne <kristine.a.du@gmail.com>
2020-06-09 10:49:08 -0600

    Update with latest.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,18 +1,44 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
 
 fetch(TRAINERS_URL)
     .then(response => response.json())
     .then(trainers => makeCards(trainers))
 
+const allPokemon = fetch(POKEMONS_URL)
+    .then(response => response.json())
+    .then(results => listPokemon(results))
+
 function makeCards(trainers){
     trainers.forEach(trainer => {
-    const card = document.createElement('div')
-    card.className = 'card'
-    card.innerHTML = `<p>${trainer.name}</p>
-    <button>Add Pokemon</button>
-    <ul></ul>`
-    document.body.append(card)
+        const card = document.createElement('div')
+
+        card.className = 'card'
+        card.setAttribute("data-id", `${trainer.id}`)
+        card.innerHTML = `<p>${trainer.name}</p>
+        <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+        <ul></ul>`
+        
+        const trainerPokemon = pokemonByTrainer(trainer, allPokemon)
+        const pokemon = document.createElement('li')
+        pokemon.innerHTML
+
+        // listPokemon(trainer)
+
+        main.append(card)
     })
+}
+
+// function listPokemon(pokemons){
+//     // pokemonByTrainer(trainer,pokemons)
+
+// }
+
+function listPokemon(pokemons){
+    pokemons.map(pokemon => {return [].push(pokemon)})
+}
+function pokemonByTrainer(trainer,pokemons){
+    pokemons.filter(pokemon => pokemon.trainer_id == 1)
 }
